3fdf58ca40480fa8c906f84a7950f7f81bc0de13
Paramasivam-n <n.paramasivam.cse@gmail.com>
2023-10-19 19:06:10 +0530

    fix some issue

--- src/pages/Questions/QuestionsDetails.jsx
+++ src/pages/Questions/QuestionsDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams, Link, useNavigate, useLocation } from "react-router-dom";
+import { useParams, Link, useNavigate} from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import moment from "moment";
 import copy from "copy-to-clipboard";
@@ -75,8 +75,7 @@ const QuestionsDetails = () => {
     const dispatch = useDispatch();
     const [Answer, setAnswer] = useState("");
     const User = useSelector((state) => state.currentUserReducer);
-    const location = useLocation();
-    const url = "http://localhost:3000";
+   
 
     const handlePostAns = (e, answerLength) => {
         e.preventDefault();
